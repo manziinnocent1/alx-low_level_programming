48c96c247144157559c242f4cfc781e765d8b28a
manziinnocent1 <manziinnocent202@gmail.com>
2022-12-21 23:09:40 -0800

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,8 +1,4 @@
-/*
- * File: 0-strcat.c
- * Auth: Brennan D Baraban
- */
-#include ^~~~
+#include main.h
 /**
  * strcat - Concatenates the string pointed to by @src, including the terminating
  *          null byte, to the end of the string pointed to by @dest.
